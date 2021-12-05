5d0b72f6dcf1e9ec95f4dab3bde0eb625f478673
37529 <VadzimKuzmenka@mail.ru>
2021-12-05 21:06:13 +0300

    task complete

---
+++ src/task2_3.java
@@ -0,0 +1,18 @@
+public class task2_3 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 1;
+        int mas[][] = {{2, 3, 1},
+                        {2, 6, 7},
+                        {1, 3, 6}};
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[a][i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i][b] + " ");
+        }
+
+            }
+        }
+
