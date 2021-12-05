1c58e614f18d3bc3af8b06b75ff686b6ba3dc2d1
37529 <VadzimKuzmenka@mail.ru>
2021-12-05 18:04:59 +0300

    task complete

--- src/task1_8.java
+++ src/task1_8.java
@@ -1,2 +1,38 @@
 public class task1_8 {
+
+    public static void main(String[] args) {
+        int[] mas = { 1, 2, 0, -12, 5, 6, 7, -12, -10, -12, -9 };
+        System.out.println("Исходный массив");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
+        }
+        System.out.println();
+
+        int min = mas[0];
+        int counter = 0;
+
+        for (int i = 0; i < mas.length; i++) {//
+            if (mas[i] < min) {
+                min = mas[i];
+                counter = 0;
+            }
+            if (mas[i] == min) {//
+                counter++;
+            }
+        }
+
+        int[] mas1 = new int[mas.length - counter];//
+
+        for (int i = 0, j = 0; i < mas.length; i++) {//
+            if (mas[i] != min) {
+                mas1[j] = mas[i];
+                j++;
+            }
+        }
+        System.out.println("Конечный массив массив");
+        for (int i = 0; i < mas1.length; i++) {
+            System.out.print(mas1[i] + " ");
+        }
+    }
+
 }
