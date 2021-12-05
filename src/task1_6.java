6f21ad24f045f5f05210ad34ed44a2691029bf53
37529 <VadzimKuzmenka@mail.ru>
2021-12-05 16:04:19 +0300

    task complete

--- src/task1_6.java
+++ src/task1_6.java
@@ -1,11 +1,19 @@
 public class task1_6 {
     public static void main(String[] args) {
         int i;
-        int[] mas = new int[]{10, 12, 1, 78, 0, 61, 2, 78, 12, 14, 0, 0};
+        int max;
+        int sum = 0;
+        int[] mas = new int[]{10, 12, 1, 78, 0, 61, 2, 78, 13, 14, 0};
         for (i = 0; i < mas.length; i++) {
-            if (i % 2 != 0 && i % 3 !=0) {
-                System.out.println(mas[i]);
+            if (i == 1 | i == 2 | i == 3 || i % 2 != 0 & i % 3 != 0) {
+                sum = sum + mas[i];
+                System.out.println("наростояющая сумма элементов с простым числом в качестве индекса  =" + sum);
             }
         }
     }
 }
+
+
+
+
+
